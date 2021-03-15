cd37c9292a43c6570340fd166eed0ba0a9ad61a3
Felipe Silva <felipe.dacs@gmail.com>
2021-03-14 23:18:53 -0300

    :100:(recepcao/atendimento) lança not present e não validation

--- UseCases/CadastroAtendimentoRecepcao/CadastroAtendimentoRecepcaoDTO.php
+++ UseCases/CadastroAtendimentoRecepcao/CadastroAtendimentoRecepcaoDTO.php
@@ -2,7 +2,10 @@
 
 namespace Core\UseCases\CadastroAtendimentoRecepcao;
 
-use Core\Exceptions\Validations\ValidationException;
+use Core\Exceptions\Validations\{
+    ValidationException,
+    NotPresentException,
+};
 
 use Core\Models\Usuario\Usuario;
 use Core\Models\Recepcao\Atendimento;
@@ -13,6 +16,6 @@ class CadastroAtendimentoRecepcaoDTO
 
     public function valida()
     {
-        throw new ValidationException('Atendimento', 'é um campo obrigatório', '');
+        throw new NotPresentException('Atendimento', 'é um campo obrigatório', '');
     }
 }
